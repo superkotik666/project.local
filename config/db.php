81219b535bc7af4a1aab1ea2d9e92e219e61ada1
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-01 18:39:30 +0200

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'host' => '127.0.0.1',
+	'name' => 'myforum',
+	'user' => 'test_user2',
+	'password' => 'password2',
+];
---
+++ config/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	
+	'' => [
+		'controller' => 'main',
+		'action' => 'reg',
+	],
+	
+	'forum' => [
+		'controller' => 'main',
+		'action' => 'send',
+	],
+	
+	
+];

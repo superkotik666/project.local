93ff6e142705e27dd669b2c1daae9eaf2b64d473
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-02 15:38:42 +0200

    Update MainController.php

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -1,24 +1,15 @@
 <?php
 
-namespace controllers;
-use core\Controller;
-use lib\Db;
-use models\Main;
+        namespace controllers;
+        use core\Controller;
+        use lib\Db;
+        use models\Main;
 
-class MainController extends Controller {
+        class MainController extends Controller {
 
 	public function regAction (){
-		
-		$this->view->render('Страница регистрации' );
-		$this->model->checkconnect();
-	}
-	public function sendAction (){
-	
-	$result = $this->model->gewNews();
-	$vars = ['news' => $result,];
-	$this->view->render('Чатьтесь))0)', $vars);
-	$this->model->checkForum();
-	
-	}
-	
-}
+	$this->view->render('Страница регистрации' );
+	$this->model->checkconnect();
+	}}
+        
+        ?>

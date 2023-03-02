7e6b67810962f41625039b9723d43c4ba55dea17
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-02 15:04:37 +0200

    Update Main.php

--- models/Main.php
+++ models/Main.php
@@ -1,74 +1,64 @@
-<?php
+        <?php
 
-namespace models;
+        namespace models;
+        use core\Model;
+        use lib\Db;
 
-use core\Model;
-use lib\Db;
-
-
-
-class Main extends Model {
+        class Main extends Model {
 
 	public function gewNews(){
         $result = $this->db->row('SELECT title, description FROM news');
         return $result;
-
-    }
-public function checkconnect (){
-    if (!empty($_POST['login']) and !empty($_POST['password'])) {
+        } 
+        public function checkconnect (){
+        if (!empty($_POST['login']) and !empty($_POST['password'])) {
         $login = $_POST['login'];
         $password = $_POST['password'];
-    $statement = $this->db->column("SELECT * FROM users WHERE login='$login'");
+        $statement = $this->db->column("SELECT * FROM users WHERE login='$login'");
     
-    if (empty($statement)) {
-    $this->db->column("INSERT INTO users SET login='$login', password='$password'");
-    $_SESSION['auth'] = true;
-    $_SESSION['login'] = $_REQUEST['login']; 
-    $addr = '/forum';
-    header("Location: $addr");
-} else {
-    echo 'Такой логин уже используется';
-}
-}
-}
+        if (empty($statement)) {
+        $this->db->column("INSERT INTO users SET login='$login', password='$password'");
+        $_SESSION['auth'] = true;
+        $_SESSION['login'] = $_REQUEST['login']; 
+        $addr = '/forum';
+        header("Location: $addr");
+        } else {
+        echo 'Такой логин уже используется';
+        }}}
 
-public function checkForum(){
-
-    if ($_SESSION['auth'] && isset($_POST['comment'])){
-                                
+        public function checkForum(){
+        if (isset($_POST['comment'])){
+        
         $username = $_SESSION['login'];
         $comment = $_POST['comment'];
         $date = date('Y-m-d H:i:s');
         $query = $this->db->query("INSERT INTO myforum.comments (username, comment, date) VALUES ('$username', '$comment', '$date')");
         
         if ($query){
-            echo 'Suxessfull';
+        echo '<pre>';
+        echo 'Successful!';
+        echo '</pre>';
         } else {
             echo 'Проблемы в коде в моменте отправки форм (написании сообщения)';
-            
-        }
-        }
+        }}
         $comments = $this->db->query("SELECT * FROM myforum.comments ORDER BY date DESC");
         
-        // ниже проблемы нет, текст отображается. Проблема выше, дело в отправке комментария
-  
+        if ($_SESSION['auth']) {
+        echo 'Пользователь авторизирован';
+        
         if ($comments){
-            foreach ($comments as $comm){
-            ?>
-            <p> <?= "{$comm['date']} {$comm['username']} оставил комментарий: '{$comm['comment']}'" ?> </p>
-            
-            <?php } } else { ?>
-            
-            <p>Здесь пока нет комментариев</p>
-            
-            <?php } 
-    
-    
-    }
-    
-    }
-
-
-?>
+        foreach ($comments as $comm){
+        ?>
+        <p> <?= "{$comm['date']} '{$comm['username']}' оставил комментарий: '{$comm['comment']}'" ?> </p>
+        <?php } } else { ?>
+        <p>Здесь пока нет комментариев</p>
+        <?php } 
+        } else { 
+        echo '<pre>';
+        echo 'Авторизация не пройдена';
+        echo '</pre>';
+        };
+        }}
+        ?>
 
     

8fd962bd4914bef45af6cbaabc1bda03f2943740
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-05 18:44:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,15 @@ use core\Model;
 use models\Main;
 
 spl_autoload_register(function($class) {
-    $path = str_replace('\\', '/', $class.'.php');
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+$path = str_replace('\\', '/', $class.'.php');
+if (file_exists($path)) {
+require $path;
+}});
 
 session_start();
-
 
 $router = new Router;
 $router->run();
 
-
-
-	
-	?>
+?>
 

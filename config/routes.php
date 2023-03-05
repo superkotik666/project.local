ac2d6b4688602fe294557fbb882bde7ac07e9b78
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-05 18:32:51 +0200

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -12,5 +12,21 @@ return [
 		'action' => 'send',
 	],
 	
+	'admin/addnews' => [
+		'controller' => 'admin',
+		'action' => 'add',
+	],
+	
+	'admin/delete' => [
+		'controller' => 'admin',
+		'action' => 'deleteUser',
+	],
+	
+	'admin/page' => [
+		'controller' => 'admin',
+		'action' => 'showUser',
+	],
 	
 ];
+
+?>

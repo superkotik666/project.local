0809a9ff651b92a0ff1afc55eb6db0c106a0015a
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-05 18:41:39 +0200

    Update Db.php

--- lib/Db.php
+++ lib/Db.php
@@ -6,39 +6,35 @@ use PDO;
 
 class Db {
 
-	protected $db;
+protected $db;
 	
-	public function __construct() {
-		$config = require 'config/db.php';
-		$this->db = new PDO('mysql:host='.$config['host'].';dbname='.$config['name'].'', $config['user'], $config['password']);
-	}
+public function __construct() {
+$config = require 'config/db.php';
+$this->db = new PDO('mysql:host='.$config['host'].';dbname='.$config['name'].'', $config['user'], $config['password']);
+}
 
-	public function query($sql, $params = []) {
-		$stmt = $this->db->prepare($sql);
-		if (!empty($params)) {
-			foreach ($params as $key => $val) {
-				if (is_int($val)) {
-					$type = PDO::PARAM_INT;
-				} else {
-					$type = PDO::PARAM_STR;
-				}
-				$stmt->bindValue(':'.$key, $val, $type);
-			}
-		}
-		$stmt->execute();
-		return $stmt;
-	}
+public function query($sql, $params = []) {
+$stmt = $this->db->prepare($sql);
+if (!empty($params)) {
+foreach ($params as $key => $val) {
+if (is_int($val)) {
+$type = PDO::PARAM_INT;
+} else {
+$type = PDO::PARAM_STR;
+}
+$stmt->bindValue(':'.$key, $val, $type);
+}}
+$stmt->execute();
+return $stmt;
+}
 
 //ДЛЯ ВЫВОДА РЯДА ЗНАЧЕНИЙ ИЗ БД
-    public function row($sql, $params = []) {
-		$result = $this->query($sql, $params);
-		return $result->fetchAll(PDO::FETCH_ASSOC);
-	}
-//ДЛЯ ВЫВОДА СТОЛБЦА ЗНАЧЕНИЙ ИЗ БД
-	public function column($sql, $params = []) {
-		$result = $this->query($sql, $params);
-		return $result->fetchColumn();
-	}
-
-
+public function row($sql, $params = []) {
+$result = $this->query($sql, $params);
+return $result->fetchAll(PDO::FETCH_ASSOC);
 }
+//ДЛЯ ВЫВОДА СТОЛБЦА ЗНАЧЕНИЙ ИЗ БД
+public function column($sql, $params = []) {
+$result = $this->query($sql, $params);
+return $result->fetchColumn();
+}}

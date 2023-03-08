1723e547dc5c906e108ca685b91445082b075c3b
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-08 20:03:50 +0200

    Update Admin.php

--- models/Admin.php
+++ models/Admin.php
@@ -1,9 +1,11 @@
-    <?php
-    namespace models;
+<?php
+namespace models;
     use core\Model;
     use lib\Db;
   
     class Admin extends Model {
+
+    public $error;
 
     public $logins; //придумать зачем здесь это свойство
     
@@ -21,15 +23,38 @@
     $this->db->query("DELETE FROM users WHERE id='$id'");
     } 
   
-    public function addNews($post){
+    public function addNews(){
     
     $title = $_POST['title'] ; 
     $description = $_POST['description'];
     
     $this->db->query("INSERT INTO news SET title ='$title', description='$description'"); 
     return $this->db->lastInsertId();
-    }}
+    }
 
-    ?>
+    
+    public function postValidate($post) {
+		$titleLen = iconv_strlen($post['title']);
+		$descriptionLen = iconv_strlen($post['description']);
+		if ($titleLen < 3 or $titleLen > 100) {
+			echo 'Заголовок должен содержать от 3 до 100 символов.';
+			return false;
+		} elseif ($descriptionLen < 3 or $descriptionLen > 300) {
+			echo 'Описание должно содержать от 3 до 300 символов.';
+            return false;
+		} 
+        return true ;
+		
+		
+	}
+    public function postDelete($id) {
+		$params = [
+			'id' => $id,
+		];
+		$this->db->query('DELETE FROM news WHERE id = :id', $params);
+        
+		
+	}
+}
 
     

1f8e97bf2c955a7c4245b32376138d1642ae3717
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-10 20:14:10 +0200

    Update reg.php

--- views/main/reg.php
+++ views/main/reg.php
@@ -1,6 +1,10 @@
 <p> СТРАНИЦА РЕГИСТРАЦИИ </p>
 <form action="" method="POST">
+<p> Придумайте логин </p>
 <input name="login">
+<p> Введите адрес электронной почты </p>
+<input name="email">
+<p> Придумайте пароль </p>
 <input name="password" type="password">
 <input type="submit">
  </form>

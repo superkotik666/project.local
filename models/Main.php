303a3362ecdaa047322b1c5dbdc9919027ff0016
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-11 18:52:12 +0200

    Update Main.php

--- models/Main.php
+++ models/Main.php
@@ -6,13 +6,11 @@
    class Main extends Model {
 
    public function checkAuth (){
-     if (!empty($_POST['login']) && !empty($_POST['password'])) {
+      if (!empty($_POST['login']) && !empty($_POST['password'])) {
         $login = $_POST['login'];
-        $password = $_POST['password'];
-        $log = $this->db->column("SELECT * FROM users WHERE login='$login'");
-        $pass = $this->db->column("SELECT * FROM users WHERE password='$password'");
-        
-     if (!empty($log) && !empty($pass)) {
+        $hash = $this->db->column("SELECT password FROM users WHERE login ='$login'");
+      if (password_verify($_POST['password'],$hash )){
+      
         $_SESSION['auth'] = true;
         $_SESSION['login'] = $_REQUEST['login']; 
         $addr = '/forum';
@@ -22,16 +20,15 @@
         echo 'Введен неправильный логин или пароль';
       }
   }
-} 
-  
-  
-  public function checkconnect (){
+}
+ 
+   public function checkconnect (){
      if (!empty($_POST['login']) && !empty($_POST['password'])) {
         $login = $_POST['login'];
-        $password = $_POST['password'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $mail = $_POST['email'];
         $statement = $this->db->column("SELECT * FROM users WHERE login='$login'");
-    
+        
     //проверка на использование вводимого логина
     
       if (empty($statement)) {

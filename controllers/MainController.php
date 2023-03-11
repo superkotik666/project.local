0e4798d0295a0926ca0b270ef48cc8e20fe21897
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-11 09:42:34 +0200

    Update MainController.php

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -1,15 +1,21 @@
 <?php
 
-        namespace controllers;
-        use core\Controller;
-        use lib\Db;
-        use models\Main;
+    namespace controllers;
+    use core\Controller;
+    use lib\Db;
+    use models\Main;
 
-        class MainController extends Controller {
+    class MainController extends Controller {
 
-	public function regAction (){
+    public function regAction (){
 	$this->view->render('Страница регистрации' );
 	$this->model->checkconnect();
-	}}
+	}
+	
+    public function authAction() {
+	$this->view->render('Вход в Пустоту' );
+	$this->model->checkconnect();	
+	}
+    }
         
         ?>

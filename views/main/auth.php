e7caf0b5c34f8232f92b67f1ac01b68cb4992520
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-11 09:40:30 +0200

    Add files via upload

---
+++ views/main/auth.php
@@ -0,0 +1,11 @@
+<h2> ФОРУМ ДЛЯ ВСЕХ ЖЕЛАЮЩИХ </h2>
+<h3> Welcome!</h3>
+<form action="" method="POST">
+<p> Введите логин </p>
+<input name="login">
+<p> Введите пароль </p>
+<input name="password" type="password">
+<input type="submit">
+ </form>
+ 
+ <p> <a href="http://framework.local/registration" target="_self">Еще не завели аккаунт? Тогда зарегистрируйтесь!</a> </p>

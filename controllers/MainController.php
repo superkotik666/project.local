2187f79372b6a603e627b9dc0e9d0632648d17e6
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-11 10:19:27 +0200

    Update MainController.php
    Add auth action

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -7,9 +7,9 @@
 
     class MainController extends Controller {
 
-    public function regAction (){
-	$this->view->render('Страница регистрации' );
-	$this->model->checkconnect();
+    public function authAction() {
+	$this->view->render('Вход в Пустоту' );
+	$this->model->checkAuth ();	
 	}
 	
     public function authAction() {

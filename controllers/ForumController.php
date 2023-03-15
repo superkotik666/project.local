a178b8a623f2d72eaf160f679970b78d73a6523b
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-03-15 20:38:14 +0200

    Update ForumController.php

--- controllers/ForumController.php
+++ controllers/ForumController.php
@@ -8,6 +8,11 @@
         class ForumController extends Controller {
 
 	public function sendAction (){
+	
+	   if(isset($_POST['buttonExit'])){
+	      $this->model->logoutAction();
+	}
+		
 	$result = $this->model->gewNews();
 	$vars = ['news' => $result,];
 	$this->view->render('Чатьтесь))0)', $vars);

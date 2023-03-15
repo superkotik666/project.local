7a2aae08f0bd3b5a97a92efd0e66fea74a294bca
SuperKotik666 <johndoe@example.com>
2023-03-15 22:12:10 +0200

    new func

--- models/Admin.php
+++ models/Admin.php
@@ -36,14 +36,14 @@ namespace models;
     public function postValidate($post) {
 		$titleLen = iconv_strlen($post['title']);
 		$descriptionLen = iconv_strlen($post['description']);
-		if ($titleLen < 3 or $titleLen > 100) {
-			echo 'Заголовок должен содержать от 3 до 100 символов.';
+		   if ($titleLen < 3 or $titleLen > 100) {
+			     echo 'Заголовок должен содержать от 3 до 100 символов.';
 			return false;
 		} elseif ($descriptionLen < 3 or $descriptionLen > 300) {
-			echo 'Описание должно содержать от 3 до 300 символов.';
-            return false;
+			     echo 'Описание должно содержать от 3 до 300 символов.';
+      return false;
 		} 
-        return true ;
+      return true ;
 		
 		
 	}
@@ -55,6 +55,11 @@ namespace models;
         
 		
 	}
+
+public function ppp(){
+  
+}
+
 }
 
     
